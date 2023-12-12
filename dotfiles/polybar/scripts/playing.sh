e9e8d96f5cf53d415e21c27812a940f9dfc08a78
Victor Bonnelle <victor.bonnelle@protonmail.com>
2023-12-12 22:11:51 +0100

    Display music on polybar from any player

--- dotfiles/polybar/scripts/playing.sh
+++ dotfiles/polybar/scripts/playing.sh
@@ -1,11 +1,18 @@
 #!/bin/bash
 
-player=$1
-
+# Get metadata
 player_not_running=false
-title=$(playerctl -p $player metadata title 2>/dev/null) || player_not_running=true
-artist=$(playerctl -p $player metadata artist 2>/dev/null) || player_not_running=true
+title=$(playerctl metadata title 2>/dev/null) || player_not_running=true
+artist=$(playerctl metadata artist 2>/dev/null) || player_not_running=true
 
+# Ensure played content is music by getting its album
+album=$(playerctl metadata album 2>/dev/null) || player_not_running=true
+if [[ -z "$album" ]]
+then
+    player_not_running=true
+fi
+
+# Format and echo output
 out=""
 if [[ $player_not_running == false ]]
 then
