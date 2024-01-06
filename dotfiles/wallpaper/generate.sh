747df532fde11cf5d1504263ec052d40d830f95b
Victor Bonnelle <victor.bonnelle@protonmail.com>
2024-01-06 21:54:51 +0100

    Add lock on lock screen wallpaper

--- dotfiles/wallpaper/generate.sh
+++ dotfiles/wallpaper/generate.sh
@@ -2,8 +2,10 @@
 
 RES=$(xdpyinfo | awk '/dimensions/{print $2}')
 BLUR=25
+LOCK_SIZE=150
 
 mkdir -p output
 convert input.jpg -resize $RES^ -gravity center -extent $RES output/desktop.jpg
-convert output/desktop.jpg -blur 0x$BLUR output/lock.jpg
+convert output/desktop.jpg -blur 0x$BLUR output/login.jpg
+convert output/login.jpg assets/lock.png -gravity center -geometry ${LOCK_SIZE}x${LOCK_SIZE}+0+0 -composite output/lock.jpg
 feh --bg-fill output/desktop.jpg
