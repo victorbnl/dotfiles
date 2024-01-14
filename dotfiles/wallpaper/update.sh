8a6cedc94bacc00d62fa55062d8efa06fe00b6b4
Victor Bonnelle <victor.bonnelle@protonmail.com>
2024-01-14 20:56:00 +0100

    Better wallpaper quality

--- dotfiles/wallpaper/update.sh
+++ dotfiles/wallpaper/update.sh
@@ -5,7 +5,7 @@ BLUR=25
 LOCK_SIZE=150
 
 mkdir -p output
-convert input.jpg -resize $RES^ -gravity center -extent $RES output/desktop.jpg
-convert output/desktop.jpg -blur 0x$BLUR output/login.jpg
-convert output/login.jpg assets/lock.png -gravity center -geometry ${LOCK_SIZE}x${LOCK_SIZE}+0+0 -composite output/lock.jpg
+convert input.jpg -quality 100 -resize $RES^ -gravity center -extent $RES output/desktop.jpg
+convert output/desktop.jpg -quality 100 -blur 0x$BLUR output/login.jpg
+convert output/login.jpg assets/lock.png -quality 100 -gravity center -geometry ${LOCK_SIZE}x${LOCK_SIZE}+0+0 -composite output/lock.jpg
 feh --bg-fill output/desktop.jpg
