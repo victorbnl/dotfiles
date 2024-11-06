a0fbdf25c72b53539cfcc0182b5b9f08b767f247
Victor Bonnelle <victor.bonnelle@protonmail.com>
2024-11-06 23:10:04 +0100

    Add comma

--- flake.nix
+++ flake.nix
@@ -4,13 +4,22 @@
 
     home-manager.url = "github:nix-community/home-manager/master";
     home-manager.inputs.nixpkgs.follows = "nixpkgs";
+
+    nix-index-database.url = "github:nix-community/nix-index-database";
+    nix-index-database.inputs.nixpkgs.follows = "nixpkgs";
   };
 
-  outputs = { nixpkgs, home-manager, ... }: {
+  outputs = { nixpkgs, home-manager, nix-index-database, ... }: {
     nixosConfigurations = {
       Victor-PC = nixpkgs.lib.nixosSystem {
         modules = [
           ./nixos/configuration.nix
+
+          nix-index-database.nixosModules.nix-index
+          {
+            programs.nix-index-database.comma.enable = true;
+          }
+
           home-manager.nixosModules.home-manager
           {
             home-manager.useGlobalPkgs = true;
