aee146c7a149e767c4f80a29843cf94a2b807f8c
Victor Bonnelle <victor.bonnelle@protonmail.com>
2024-11-07 01:52:42 +0100

    Set Material Icons as default VSCode file icon theme

--- home-manager/programs/vscode.nix
+++ home-manager/programs/vscode.nix
@@ -4,6 +4,10 @@
   programs.vscode = {
     enable = true;
 
+    userSettings = {
+      "workbench.iconTheme" = "material-icon-theme";
+    };
+
     extensions = with pkgs.vscode-extensions; [
       bbenoist.nix
       editorconfig.editorconfig
