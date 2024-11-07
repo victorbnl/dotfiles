5a16c78919ba255f06b050418ff4b345b4beb491
Victor Bonnelle <victor.bonnelle@protonmail.com>
2024-11-07 01:52:30 +0100

    Fix HM file already exists

--- flake.nix
+++ flake.nix
@@ -23,6 +23,7 @@
           home-manager.nixosModules.home-manager
           {
             home-manager.useGlobalPkgs = true;
+            home-manager.backupFileExtension = "backup";
             home-manager.users.victor = import ./home-manager/home.nix;
           }
         ];
