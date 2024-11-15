42e17e307bb5d50521488ac6958d279529dd1a6c
Victor Bonnelle <victor.bonnelle@protonmail.com>
2024-11-15 03:49:49 +0100

    Code style in flake.nix

--- flake.nix
+++ flake.nix
@@ -2,18 +2,24 @@
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
 
-    home-manager.url = "github:nix-community/home-manager/master";
-    home-manager.inputs.nixpkgs.follows = "nixpkgs";
+    home-manager = {
+      url = "github:nix-community/home-manager/master";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
 
-    nix-index-database.url = "github:nix-community/nix-index-database";
-    nix-index-database.inputs.nixpkgs.follows = "nixpkgs";
+    nix-index-database = {
+      url = "github:nix-community/nix-index-database";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
 
     nix-vscode-extensions.url = "github:nix-community/nix-vscode-extensions";
 
     nur-repository.url = "github:nix-community/NUR";
 
-    firefox-onebar.url = "https://git.gay/freeplay/Firefox-Onebar/raw/branch/waf/onebar.css";
-    firefox-onebar.flake = false;
+    firefox-onebar = {
+      url = "https://git.gay/freeplay/Firefox-Onebar/raw/branch/waf/onebar.css";
+      flake = false;
+    };
   };
 
   outputs = {
