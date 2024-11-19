67755a634b762b5f1a1a824e4247aa782a612f44
Victor Bonnelle <victor.bonnelle@protonmail.com>
2024-11-19 16:06:49 +0100

    Add rebuild-nixos alias

--- home-manager/home.nix
+++ home-manager/home.nix
@@ -22,6 +22,12 @@ in
     ./polybar.nix
   ];
 
+  programs.bash.enable = true;
+
+  home.shellAliases = {
+    "rebuild-nixos" = "sudo nixos-rebuild switch --flake path:/etc/nixos";
+  };
+
   xresources.properties = {
     "Xft.dpi" = 134;
   };
