fb1deca0e7d6275e0f99734d328c7bf04c087e41
Victor Bonnelle <victor.bonnelle@protonmail.com>
2024-11-21 00:33:11 +0100

    Add vim configuration

--- home-manager/programs/default.nix
+++ home-manager/programs/default.nix
@@ -15,4 +15,19 @@
     userEmail = "victor.bonnelle@protonmail.com";
     userName = "Victor Bonnelle";
   };
+
+  programs.vim = {
+    enable = true;
+
+    settings = {
+      expandtab = true;
+
+      number = true;
+      relativenumber = true;
+    };
+
+    extraConfig = ''
+      set laststatus=0
+    '';
+  };
 }
