2469c3c3189ac89ec24663ae649b7797337038bd
Victor Bonnelle <victor.bonnelle@protonmail.com>
2024-11-26 01:22:13 +0100

    Use VSCode custom dialogs

--- home/programs/vscode.nix
+++ home/programs/vscode.nix
@@ -19,6 +19,7 @@
     userSettings = {
       "code-runner.runInTerminal" = true;
       "git.confirmSync" = false;
+      "window.dialogStyle" = "custom";
       "window.titleBarStyle" = "custom";
       "workbench.iconTheme" = "material-icon-theme";
     };
