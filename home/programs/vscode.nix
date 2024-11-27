a02f02b4a530c1ffee95efb3f62e28febf4b2c0a
Victor Bonnelle <victor.bonnelle@protonmail.com>
2024-11-27 02:04:12 +0100

    Add VSCode background

--- home/programs/vscode.nix
+++ home/programs/vscode.nix
@@ -23,6 +23,7 @@
       "window.dialogStyle" = "custom";
       "window.experimentalControlOverlay" = false;
       "window.titleBarStyle" = "custom";
+      "workbench.colorTheme" = "Styx Material";
       "workbench.iconTheme" = "material-icon-theme";
     };
 
@@ -38,6 +39,15 @@
       pkief.material-icon-theme
       shardulm94.trailing-spaces
       tomoki1207.pdf
+
+      (hshhrr.styx.overrideAttrs (oldAttrs: {
+        postInstall = (oldAttrs.postInstall or "") + ''
+          find $out -name "styx*-color-theme.json" \
+            -exec sed -i "s/#0A3329/#303030/g" "{}" \; \
+            -exec sed -i "s/#91F2DA/#FFFFFF/g" "{}" \; \
+            -exec sed -i -E 's/("widget\.shadow": )"#000000"/\1"#202020"/g' "{}" \;
+        '';
+      }))
     ];
 
     # mostly taken from https://github.com/KatsuteDev/Background/
@@ -49,6 +59,21 @@
           /* Hide window controls */
           .window-controls-container { width: 4px !important; }
           .window-controls-container > * { display: none !important; }
+
+          /* Background */
+          body::after{
+            content: "";
+            display: block;
+            position: absolute;
+            z-index: 1000;
+            inset: 0;
+            background-image: url("vscode-file://vscode-app/home/victor/.vscode-background-image.jpg");
+            background-size: cover;
+            background-position: center;
+            pointer-events: none;
+            opacity: 0.2;
+            mix-blend-mode: lighten;
+          }
         EOF
         checksum="$(sha256sum -b "$workbenchPath" | xxd -r -p | base64 | sed "s/=*$//g")"
         sed -i -E 's|(^\s*"vs\/workbench\/workbench\.desktop\.main\.css": ")[^"]*("),|\1'$checksum'\2,|' "$out/lib/vscode/resources/app/product.json"
