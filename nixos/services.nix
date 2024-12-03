95c3de3a44d5659eb208581922fec73c4b2fce91
Victor Bonnelle <victor.bonnelle@protonmail.com>
2024-12-03 16:17:38 +0100

    Code style

--- nixos/services.nix
+++ nixos/services.nix
@@ -10,6 +10,7 @@
     power-profiles-daemon.enable = false;
     tlp = {
       enable = true;
+
       settings = {
         CPU_SCALING_GOVERNOR_ON_BAT = "powersave";
         CPU_SCALING_GOVERNOR_ON_AC = "performance";
