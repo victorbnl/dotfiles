8cd2a723bb5c675606e24d6ea2b782f3be05902c
Victor Bonnelle <victor.bonnelle@protonmail.com>
2024-12-20 03:44:36 +0100

    Add VSCode build task

--- home/default.nix
+++ home/default.nix
@@ -3,7 +3,6 @@
     ./desktop
     ./programs
 
-    ./shell.nix
     ./software.nix
   ];
 
--- home/shell.nix
+++
@@ -1,9 +0,0 @@
-{
-  programs.bash = {
-    enable = true;
-
-    shellAliases = {
-      "rebuild-nixos" = "sudo nixos-rebuild switch --flake path:/data/victor/dotfiles";
-    };
-  };
-}
