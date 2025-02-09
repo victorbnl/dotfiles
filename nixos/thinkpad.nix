abcb1cfdff2ac994bfc847dccc4dbf09fc868a3e
Victor Bonnelle <victor.bonnelle@protonmail.com>
2025-02-09 19:37:19 +0100

    More efficient LED disabling

--- nixos/thinkpad.nix
+++ nixos/thinkpad.nix
@@ -1,18 +1,28 @@
+{ pkgs, ... }:
+
 {
   imports = [ ./common ];
 
   networking.hostName = "Victor-ThinkPad";
 
-  systemd.services.disable-leds = {
-    wantedBy = [ "multi-user.target" ];
-    script = ''
-      echo 0 > "/sys/class/leds/platform::mute/brightness"
-      echo 0 > "/sys/class/leds/platform::micmute/brightness"
-      while true
-      do
-        echo 0 > "/sys/class/leds/tpacpi::power/brightness"
-        sleep 1
-      done
-    '';
+  systemd = {
+    tmpfiles.rules = [
+      "w /sys/class/leds/platform::mute/brightness - - - - 0"
+      "w /sys/class/leds/platform::micmute/brightness - - - - 0"
+    ];
+
+    services.disable-power-led = {
+      wantedBy = [ "multi-user.target" ];
+
+      script = ''
+        nice -n 19 /bin/sh ${pkgs.writeText "disable-power-led.sh" ''
+          while true
+            do
+              echo 0 > /sys/class/leds/tpacpi::power/brightness
+              sleep 5
+            done
+        ''}
+      '';
+    };
   };
 }
