64905191fa21ae3c34056be604d438bdc216a613
Victor Bonnelle <victor.bonnelle@protonmail.com>
2025-03-24 17:09:31 +0100

    Generate i3 keybindings

--- home/desktop/i3.nix
+++ home/desktop/i3.nix
@@ -1,4 +1,4 @@
-{ pkgs, ... }:
+{ pkgs, lib, ... }:
 
 {
   xsession.windowManager.i3 = {
@@ -91,29 +91,20 @@
         "${modifier}+Shift+Down" = "move down";
         "${modifier}+Shift+Up" = "move up";
         "${modifier}+Shift+Right" = "move right";
-
-        "${modifier}+1" = "workspace number 1";
-        "${modifier}+2" = "workspace number 2";
-        "${modifier}+3" = "workspace number 3";
-        "${modifier}+4" = "workspace number 4";
-        "${modifier}+5" = "workspace number 5";
-        "${modifier}+6" = "workspace number 6";
-        "${modifier}+7" = "workspace number 7";
-        "${modifier}+8" = "workspace number 8";
-        "${modifier}+9" = "workspace number 9";
-        "${modifier}+0" = "workspace number 10";
-
-        "${modifier}+Shift+1" = "move container to workspace number 1";
-        "${modifier}+Shift+2" = "move container to workspace number 2";
-        "${modifier}+Shift+3" = "move container to workspace number 3";
-        "${modifier}+Shift+4" = "move container to workspace number 4";
-        "${modifier}+Shift+5" = "move container to workspace number 5";
-        "${modifier}+Shift+6" = "move container to workspace number 6";
-        "${modifier}+Shift+7" = "move container to workspace number 7";
-        "${modifier}+Shift+8" = "move container to workspace number 8";
-        "${modifier}+Shift+9" = "move container to workspace number 9";
-        "${modifier}+Shift+0" = "move container to workspace number 10";
-      };
+      }
+      // builtins.foldl'
+        (set: args:
+          set // builtins.listToAttrs (builtins.genList (i:
+            let ws = i + 1;
+            in {
+              name = "${args.mod}+${toString (lib.trivial.mod ws 10)}";
+              value = "${args.cmd} ${toString ws}";
+            }) 10))
+        {}
+        [
+          { mod = modifier; cmd = "workspace number"; }
+          { mod = "${modifier}+Shift"; cmd = "move container to workspace number"; }
+        ];
 
       bars = [];
     };
