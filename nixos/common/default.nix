dbf6a076d046de1cc6b07d93844aa5b3c67edf60
Victor Bonnelle <victor.bonnelle@protonmail.com>
2025-03-27 00:46:41 +0100

    Add devenv keys

--- nixos/common/default.nix
+++ nixos/common/default.nix
@@ -62,5 +62,10 @@
     enable32Bit = true;
   };
 
+  nix.extraOptions = ''
+    extra-substituters = https://devenv.cachix.org
+    extra-trusted-public-keys = devenv.cachix.org-1:w1cLUi8dv3hnoSPGAuibQv+f9TZLr6cv/Hm9XgU50cw=
+  '';
+
   system.stateVersion = "24.05";
 }
