f99be4b082eceea388ca249813673e0d209a69be
Victor Bonnelle <victor.bonnelle@protonmail.com>
2025-04-18 05:00:51 +0200

    Fix SDDM wallpaper path

--- nixos/common/sddm.nix
+++ nixos/common/sddm.nix
@@ -1,10 +1,10 @@
-{ pkgs, ... }:
+{ pkgs, root, ... }:
 
 {
   environment.systemPackages = [
     (pkgs.sddm-chili-theme.overrideAttrs(oldAttrs: {
       preInstall = (oldAttrs.preInstall or "") + ''
-        sed -i 's|^background=.*$|background=${../../wallpapers/dm}|' theme.conf
+        sed -i 's|^background=.*$|background=${../../wallpapers/display-manager}|' theme.conf
       '';
     }))
   ];
