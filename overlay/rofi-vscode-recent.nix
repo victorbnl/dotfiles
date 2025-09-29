82cce087019f0621fa8d653c18b7b451d27361bb
Marie Bonnelle <marie.bonnelle@proton.me>
2025-09-29 16:26:15 +0200

    Constraint rofi-vscode-recent to ~/dev

--- overlay/rofi-vscode-recent.nix
+++ overlay/rofi-vscode-recent.nix
@@ -18,14 +18,16 @@ writeShellApplication {
   bashOptions = [];
 
   text = ''
+    FOLDER="$HOME/dev"
+
     function process() {
       while read -r line; do
-        [ -d "$line" ] && echo -e "$line\0icon\x1fvscode"
+        [ -d "$line" ] && [[ "$line" = "$FOLDER/"* ]] && echo -e "''${line##"$FOLDER/"}\0icon\x1fvscode"
       done
     }
 
     choice="$(vscode-recent --output-format absolute-path | process | rofi -dmenu)"
 
-    [ -n "$choice" ] && code --new-window "$choice"
+    [ -n "$choice" ] && code --new-window "$FOLDER/$choice"
   '';
 }
